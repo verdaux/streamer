b81b5dc4d49525f924857f63509f90c626fe0b1d
PB <xachineon@gmail.com>
2025-06-17 17:54:03 +0530

    Initial commit for Streamer project for Kafka

---
+++ src/test/java/com/streamer/streamer/StreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamer.streamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
